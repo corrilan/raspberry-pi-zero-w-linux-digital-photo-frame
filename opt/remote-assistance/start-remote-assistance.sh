b793ed89420b72654e945b9d9eb87a32929603ec
corrilan <33758018+corrilan@users.noreply.github.com>
2021-11-18 21:48:46 +0000

    Update start-remote-assistance.sh

--- opt/remote-assistance/start-remote-assistance.sh
+++ opt/remote-assistance/start-remote-assistance.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 . /home/pi/.bashrc
-/usr/bin/screen -S reverse-ssh-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 13333:localhost:22 frame@frame.enframed.co.uk
-/usr/bin/screen -S reverse-vnc-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 13332:localhost:5900 frame@frame.enframed.co.uk
-/usr/bin/screen -S reverse-web-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 13331:localhost:80 -N -p 2222 frame@frame.enframed.co.uk
-/usr/bin/screen -S x11vnc -c /etc/.reverse-ssh-screen -d -m /usr/bin/sudo -u pi /usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -forever
+
+if ! screen -ls | grep -q "reverse"
+    then
+	    /usr/bin/screen -S reverse-ssh-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 11113:localhost:22 user@frame.yourdomain.com
+	    /usr/bin/screen -S reverse-vnc-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 11112:localhost:5900 user@frame.yourdomain.com
+	    /usr/bin/screen -S reverse-web-enframed -c /etc/.reverse-ssh-screen -d -m autossh -R 11111:localhost:80 -N -p 2222 user@frame.yourdomain.com
+	    /usr/bin/screen -S x11vnc -c /etc/.reverse-ssh-screen -d -m /usr/bin/sudo -u pi /usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -forever
+    else echo "Screen Currently Running"
+fi
